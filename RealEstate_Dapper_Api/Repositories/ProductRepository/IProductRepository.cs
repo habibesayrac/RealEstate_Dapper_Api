b22e7dc42ba7db5b3eaeb998ccb8f0fcd6ebbc14
habib <habib@LENOVO>
2025-04-14 09:19:31 +0300

    DefaultLayout Added

---
+++ RealEstate_Dapper_Api/Repositories/ProductRepository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using RealEstate_Dapper_Api.Models.Dtos.ProductDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.ProductRepository
+{
+    public interface IProductRepository
+    {
+        Task<List<ResultProductDto>> GetAllProductAsync();
+        Task<List<ResultProductWithCategoryDto>> GetAllProductWithCategoryAsync();
+        //void CreateCategory(CreateCategoryDto categoryDto);
+        //void DeleteCategory(int id);
+        //void UpdateCategory(UpdateCategoryDto categoryDto);
+        //Task<GetByIDCategoryDto> GetCategory(int id);
+    }
+}
