07d1ca1ccb52798e11e4e9fce3899e57c7fcb1a9
habib <habib@LENOVO>
2025-04-17 14:29:39 +0300

    Testimonial area added

---
+++ RealEstate_Dapper_Api/Repositories/TestimonialRepositories/ITestimonialRepository.cs
@@ -0,0 +1,10 @@
+﻿using RealEstate_Dapper_Api.Models.Dtos.TestimonialDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.TestimonialRepositories
+{
+    public interface ITestimonialRepository
+    {
+        Task<List<ResultTestimonialDto>> GetAllTestimonialAsync();
+
+    }
+}
